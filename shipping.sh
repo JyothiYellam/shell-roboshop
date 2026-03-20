30ddf03815d01ec825a8a3957a3a3705134beaaa
JyothiYellam <jyothiym23@gmail.com>
2026-03-20 18:31:52 +0530

    actions

--- shipping.sh
+++ shipping.sh
@@ -68,9 +68,9 @@ VALIDATE $? "Installing MySQL"
 mysql -h $MYSQL_HOST -uroot -pRoboShop@1 -e 'use cities'
 if [ $? -ne 0 ]; then
 
-    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql &>>$LOGS_FILE
-    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/app-user.sql &>>$LOGS_FILE
-    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$LOGS_FILE
+    mysql -h mysql.jyothiy.online -uroot -pRoboShop@1 < /app/db/schema.sql
+    mysql -h mysql.jyothiy.online -uroot -pRoboShop@1 < /app/db/app-user.sql
+    mysql -h mysql.jyothiy.online -uroot -pRoboShop@1 < /app/db/master-data.sql
     VALIDATE $? "Loaded data into MySQL"
 else
     echo -e "data is already loaded ... $Y SKIPPING $N"
